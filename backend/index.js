988a4141a80ff976048d98064c5693136cf3f68f
roko123477 <78839525+roko123477@users.noreply.github.com>
2024-01-31 10:50:50 +0530

    made the nutrionix api part

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const Food = require("./models/food");
 const fetch = require("node-fetch");
-//import fetch from 'node-fetch';
+const dotenv = require("dotenv");
+dotenv.config();
 mongoose.set("strictQuery", true);
-const db_url =
-  "mongodb+srv://rohitkoner5:roko@cluster0.wapat9h.mongodb.net/?retryWrites=true&w=majority";
+const db_url = process.env.DB_URL;
+
 mongoose
   .connect(db_url)
   .then(() => console.log("database connected"))
@@ -21,67 +22,69 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.get("/", async (req, res) => {
-  res.json({ query: true });
-});
 app.get("/search/query", async (req, res) => {
   const str = req.query.str;
 
-  const db_data = await Food.find({});
-  const foodarr = db_data.filter((obj) => obj.food_name.includes(str));
-  if (foodarr.length === 0) {
-    try {
-      const fetchData = await fetch(
-        `https://trackapi.nutritionix.com/v2/search/instant/?query=${str}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "x-app-id": "69bdb8bc",
-            "x-app-key": "481c8dad73f91dab9886507af702619b",
-          },
-        }
-      );
-      const fetchjson = await fetchData.json();
-   
-       
-      fetchjson.common.map(async (obj) => {
-        const fetchCalorie = await fetch(
-          "https://trackapi.nutritionix.com/v2/natural/nutrients",
+  if (!str) {
+    res.status(400).json({ msg: "Enter string parameter ||" });
+  } else {
+    // filtering foodname that includes already filtered names
+    const foodarr = await Food.find({ food_name: { $regex: str } });
+    // if new name comes
+    if (foodarr.length === 0) {
+      // fetching the details from nutrionix api
+      try {
+        const fetchData = await fetch(
+          `https://trackapi.nutritionix.com/v2/search/instant/?query=${str}`,
           {
-            method: "POST",
             headers: {
               "Content-Type": "application/json",
-              "x-app-id": "69bdb8bc",
-              "x-app-key": "481c8dad73f91dab9886507af702619b",
+              "x-app-id": process.env.NUTRIONIX_API_ID,
+              "x-app-key": process.env.NUTRIONIX_API_KEY,
             },
-            body: JSON.stringify({
-              query: obj.food_name,
-            }),
           }
         );
-        const fetchjsoncalorie = await fetchCalorie.json();
-   
-          try {
-            const foodCart= new Food({
-                food_name:obj.food_name,
-                photo:obj.photo.thumb,
-                calories:fetchjsoncalorie.foods[0].nf_calories
-            })
+        const fetchjson = await fetchData.json();
 
-           const doc=await foodCart.save();
-           console.log(doc);
+        const data = [];
+        for (obj of fetchjson.common.slice(0, 5)) {
+          const fetchCalorie = await fetch(
+            "https://trackapi.nutritionix.com/v2/natural/nutrients",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "x-app-id": "69bdb8bc",
+                "x-app-key": "481c8dad73f91dab9886507af702619b",
+              },
+              body: JSON.stringify({
+                query: obj.food_name,
+              }),
+            }
+          );
+          const fetchjsoncalorie = await fetchCalorie.json();
+
+          try {
+            const foodCart = new Food({
+              food_name: obj.food_name,
+              photo: obj.photo.thumb,
+              calories: fetchjsoncalorie.foods[0].nf_calories,
+            });
+
+            const doc = await foodCart.save();
+            console.log(doc);
+            data.push(doc);
           } catch (error) {
-            res.json(error)
+            res.json(error);
           }
-       
-      });
-      res.json(fetchjson);
-    } catch (error) {
-      res.json({ success: false, error: error });
+        }
+        res.status(200).json(data);
+      } catch (error) {
+        res.json({ success: false, error: error });
+      }
+    } else {
+      res.status(200).json(foodarr);
     }
-  }
-  else{
-    res.status(200).json(foodarr);
   }
 });
 
